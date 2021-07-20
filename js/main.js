76f0eb5e3cc470587932f947e71ef1de922680a6
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-07-20 12:45:07 -0300

    contact css

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 var styleContact = document.querySelectorAll('#contact__link');
-
-var styleContactSpan = document.querySelector('.contact__list li span');
-console.log(styleContactSpan);
+var styleContactSpan = document.querySelectorAll('.contact__list li span');
 
 styleContact[0].onmouseover = function () {
-    effectContactON('#4C4C4C'); 
+    effectContactON('#4C4C4C','0'); 
 };
-styleContact[0].onmouseout = effectContactOFF; 
+styleContact[0].onmouseout = function () {
+    effectContactOFF('0'); 
+};
+styleContact[1].onmouseover = function () {
+    effectContactON('#D5285E','1'); 
+};
+styleContact[1].onmouseout = function () {
+    effectContactOFF('1'); 
+};
+styleContact[2].onmouseover = function () {
+    effectContactON('#0079B9','2'); 
+};
+styleContact[2].onmouseout = function () {
+    effectContactOFF('2'); 
+};
+styleContact[3].onmouseover = function () {
+    effectContactON('#FD5956','3'); 
+};
+styleContact[3].onmouseout = function () {
+    effectContactOFF('3'); 
+};
 
-// TALVEZ COLOCAR UM PARAMETRO NO styleContactSpan[PARAMETRO]
-function effectContactON(parametro) {
-        styleContactSpan.classList.toggle("contact__span--JS");
-        styleContactSpan.style.transition = '0.5s ease';
-        styleContactSpan.style.backgroundColor = parametro; 
-        console.log(styleContactSpan.getAttribute('class'));
+function effectContactON(color,spanNumber) {
+        styleContactSpan[spanNumber].classList.toggle("contact__span--JS");
+        styleContactSpan[spanNumber].style.transition = '0.5s ease';
+        styleContactSpan[spanNumber].style.backgroundColor = color; 
 }
-function effectContactOFF() {
-    styleContactSpan.setAttribute('class', ' ');
-    console.log(styleContactSpan.getAttribute('class'));
-    styleContactSpan.style.backgroundColor = '#ffffff'; 
+function effectContactOFF(spanNumber) {
+    styleContactSpan[spanNumber].setAttribute('class', ' ');
+    styleContactSpan[spanNumber].style.backgroundColor = '#ffffff'; 
 }
