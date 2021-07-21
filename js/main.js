9cf9266156728661619e96298c24d33f6d654595
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-07-21 13:00:10 -0300

    media queries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Header
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('.header__nav');
+    if(window.scrollY>500){
+        header.style.backgroundColor = '#2979DA';
+    }else{
+        header.style.backgroundColor = 'transparent';
+    }
+});
+var navMobile = document.querySelector('.nav__Mobile');
+
+navMobile.onclick = function (){
+    var listMobile = document.querySelector('.nav__list');
+    
+    navMobile.classList.toggle("imgfechar");
+    listMobile.classList.toggle("nav__ativo");
+}
+
+// Contact
 var styleContact = document.querySelectorAll('#contact__link');
 var styleContactSpan = document.querySelectorAll('.contact__list li span');
 
@@ -35,3 +54,9 @@ function effectContactOFF(spanNumber) {
     styleContactSpan[spanNumber].setAttribute('class', ' ');
     styleContactSpan[spanNumber].style.backgroundColor = '#ffffff'; 
 }
+
+// Footer
+var thisYear = document.querySelector('.footer__text span');
+var data = new Date();
+var ano = data.getFullYear();
+thisYear.innerHTML = `© ${ano}`;
