d29a8ba9e90bdae9b56111a247341ab86da9445b
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-11-05 11:00:13 -0300

    Finalizando Projeto

--- js/main.js
+++ js/main.js
@@ -11,12 +11,14 @@ window.addEventListener('scroll', function () {
 var navMobile = document.querySelector('.nav__Mobile');
 var listMobile = document.querySelector('.nav__list');
 
+
 navMobile.onclick = function () {
     navMobile.classList.toggle("imgfechar");
     listMobile.classList.toggle("nav__ativo");
 }
 
 var navMobileLink = document.querySelectorAll('.nav__list li a');
+var contactBtn = document.querySelector('.nav__contact');
 
 function hideBar(){
     listMobile.setAttribute('class', 'nav__list');
@@ -27,6 +29,13 @@ navMobileLink[0].onclick =  hideBar;
 navMobileLink[1].onclick =  hideBar;
 navMobileLink[2].onclick =  hideBar;
 navMobileLink[3].onclick =  hideBar;
+
+contactBtn.onclick = function () {
+    if (listMobile.getAttribute('class')==='nav__list nav__ativo') {
+        hideBar()
+    }
+} 
+
 
 // Contact
 var styleContact = document.querySelectorAll('#contact__link');
