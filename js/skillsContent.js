459b6f1e24970ffd06bcb270a506a41ee5626fac
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-12-18 13:36:30 -0300

    Conteúdo Novo

---
+++ js/skillsContent.js
@@ -0,0 +1,42 @@
+const skillCard = [
+    {
+        nome: 'Adivinhe o número',
+        img: 'img/projects01.jpg',
+        text: 'Projeto JavaScript, HTML e CSS do jogo "Guess the game". No projeto haverá um número aleatório de 1 a 100 que você terá que adivinhar. A cada tentativa aparecerá uma mensagem indicando se o seu palpite foi muito alto ou baixo.'
+    },
+    {
+        nome: 'Primeiro Portfólio',
+        img: 'img/projects02.jpg',
+        text: 'Este é o meu primeiro portfólio. Desenvolvido por conta própria. Ele é responsivo, aos meus testes. Possui apenas o básico de JS. One page site, como este portfólio, possui algumas semelhanças com este projeto.'
+    },
+    {
+        nome: 'Fã Page',
+        img: 'img/projects03.jpg',
+        text: 'Projeto inspirado na tarefa do site <a href="https://www.freecodecamp.org/diogo_lima_carvalho" target="_blank">@freecodecamp</a> que estou cursando. É basicamente uma página de divulgação da cantora, com algumas informações, suas redes e álbuns. Um one page site utilizando HTML, CSS e Javascript para o desenvolvimento projeto. '
+    },
+    {
+        nome: 'Lista de Tarefa',
+        img: 'img/projects04.jpg',
+        text: 'Projeto Lista de Tarefas ou To Do List. Desenvolvido com JavaScript, HTML e CSS. O projeto utiliza Local Storage que armazena os dados no navegador do usuário, para predefinir o tema do projeto. O projeto pode ajudar na organização, em lembretes e tarefas.'
+    },
+    {
+        nome: 'Jogo da Memória',
+        img: 'img/projects05.jpg',
+        text: 'Projeto Lista de Tarefas ou To Do List. Desenvolvido com JavaScript, HTML e CSS. O projeto utiliza Local Storage que armazena os dados no navegador do usuário, para predefinir o tema do projeto. O projeto pode ajudar na organização, em lembretes e tarefas.'
+    },
+    {
+        nome: 'E-commerce',
+        img: 'img/projects06.jpg',
+        text: 'Projeto Lista de Tarefas ou To Do List. Desenvolvido com JavaScript, HTML e CSS. O projeto utiliza Local Storage que armazena os dados no navegador do usuário, para predefinir o tema do projeto. O projeto pode ajudar na organização, em lembretes e tarefas.'
+    },
+    {
+        nome: 'Converter Texto',
+        img: 'img/projects07.jpg',
+        text: 'Projeto Lista de Tarefas ou To Do List. Desenvolvido com JavaScript, HTML e CSS. O projeto utiliza Local Storage que armazena os dados no navegador do usuário, para predefinir o tema do projeto. O projeto pode ajudar na organização, em lembretes e tarefas.'
+    },
+    {
+        nome: 'Validação de Formulários',
+        img: 'img/projects08.jpg',
+        text: 'Projeto Lista de Tarefas ou To Do List. Desenvolvido com JavaScript, HTML e CSS. O projeto utiliza Local Storage que armazena os dados no navegador do usuário, para predefinir o tema do projeto. O projeto pode ajudar na organização, em lembretes e tarefas.'
+    }
+]
